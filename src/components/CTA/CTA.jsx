e8fc02626c22228cc85e9dc8b72fdb11acaa169c
jps-wilson <jwilson316@my.bcit.ca>
2026-06-23 13:56:33 -0700

    feat: CTA section with icons

--- src/components/CTA/CTA.jsx
+++ src/components/CTA/CTA.jsx
@@ -1,3 +1,23 @@
+import styles from "./CTA.module.css";
+import { FaDiscord } from "react-icons/fa";
+import { PiSparkleFill } from "react-icons/pi";
+
 export default function CTA() {
-  return <div></div>;
+  return (
+    <section className={styles.cta}>
+      <div className={styles.buttons}>
+        <a href='#' className={`${styles.button} ${styles.discord}`}>
+          <FaDiscord />
+          Join us on Discord
+        </a>
+        <a href='#' className={`${styles.button} ${styles.launch}`}>
+          <PiSparkleFill />
+          Soft Launch Opening
+        </a>
+      </div>
+      <p className={styles.tagline}>
+        Be a part of something bigger than each one of us...
+      </p>
+    </section>
+  );
 }
