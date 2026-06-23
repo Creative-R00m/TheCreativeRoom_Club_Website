1b6d756c05090dd6a8299c1c5ef5eec33cf283cc
jps-wilson <jwilson316@my.bcit.ca>
2026-06-23 13:08:27 -0700

    feat: hero section layout and styles

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Hero from "./components/Hero/Hero";
+import About from "./components/About/About";
+import CTA from "./components/CTA/CTA";
+import Footer from "./components/Footer/Footer";
+
+export default function App() {
+  return (
+    <>
+      <Hero />
+      <About />
+      <CTA />
+      <Footer />
+    </>
+  );
+}
