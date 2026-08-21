dd173d6a77f431f59f11040f9983ad854d28219c
emmpup <emisola@my.bcit.ca>
2026-08-21 08:37:36 -0700

    feature: add shadcn button component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,14 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
     <div>
       <h1>The Creative Room</h1>
+      <Button variant='default' size='default'>
+        Click Me
+      </Button>
     </div>
   );
 }
