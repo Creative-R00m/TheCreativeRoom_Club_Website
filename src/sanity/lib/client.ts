bb9e1d25bd58a2590b6d5f8be9e6e8b7aff76448
emmpup <emisola@my.bcit.ca>
2026-08-31 08:12:46 -0700

    test: display sanity content in front end

--- src/sanity/lib/client.ts
+++ src/sanity/lib/client.ts
@@ -1,10 +1,10 @@
-import { createClient } from 'next-sanity'
+import { createClient } from "next-sanity";
 
-import { apiVersion, dataset, projectId } from '../env'
+import { apiVersion, dataset, projectId } from "../env";
 
 export const client = createClient({
-  projectId,
-  dataset,
-  apiVersion,
-  useCdn: true, // Set to false if statically generating pages, using ISR or tag-based revalidation
-})
+  projectId: "ee2tq917",
+  dataset: "production",
+  apiVersion: "2026-05-15",
+  useCdn: false, // Set to false if statically generating pages, using ISR or tag-based revalidation
+});
