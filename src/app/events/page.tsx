0e05ca30b3fafe48ad4bba2a61e479a7daa3a92e
emmpup <emisola@my.bcit.ca>
2026-09-14 10:33:54 -0700

    chore: revalidates events page every 60 sec

--- src/app/events/page.tsx
+++ src/app/events/page.tsx
@@ -1,7 +1,7 @@
 import { fetchUpcomingEvents } from "@/sanity/lib/queries";
-import Image from "next/image";
-import { urlFor } from "@/sanity/lib/image";
 import { EventCard } from "@/components/ui/eventCard";
+
+export const revalidate = 60;
 
 export default async function Events() {
   const events = await fetchUpcomingEvents(2);
@@ -10,7 +10,7 @@ export default async function Events() {
     <div>
       <h1>Events</h1>
       {events.length === 0 ? (
-        <p>No published events yet.</p>
+        <p>No upcoming events yet.</p>
       ) : (
         events.map((event, index) => (
           <div key={event._id}>
