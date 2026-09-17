94e4010828e4e355d68e97212453fe51751fb42c
emmpup <emisola@my.bcit.ca>
2026-09-17 11:33:13 -0700

    style: update button styles to reflect the design

--- src/components/ui/contact-form.tsx
+++ src/components/ui/contact-form.tsx
@@ -137,7 +137,7 @@ export function ContactForm() {
         />
 
         <Button type='submit' disabled={isPending}>
-          {isPending ? "Sending..." : "Send"}
+          {isPending ? "Sending..." : "Send Message"}
         </Button>
 
         {state.message && (
