279df75be682575ace7662150aaf95443f43d3d0
emmpup <emisola@my.bcit.ca>
2026-09-25 11:19:32 -0700

    feature/ add events and section title text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,44 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 import { Button } from "@/components/ui/button";
+import { fetchUpcomingEvents } from "@/sanity/lib/queries";
+import { EventCard } from "@/components/ui/eventCard";
+import { ArrowRight } from "lucide-react";
 
-export default function Home() {
+export default async function Home() {
+  const events = await fetchUpcomingEvents(4);
+
   return (
     <div className='px-[var(--site-margin-x)]'>
-      <h1>The Creative Room</h1>
-      <Button variant='default' size='default' radius='oval'>
-        Click Me
-      </Button>
+      <div className={styles.hero}>
+        <h1>The Creative Room</h1>
+      </div>
+      <div className={styles.events}>
+        <div>
+          <div>
+            <h3 className='type-eyebrow'>Come Hang Out</h3>
+            <h2 className='type-h2'>Upcoming Events</h2>
+          </div>
+          <div className={styles.eventsButton}>
+            <Button size='sm'>
+              <a href='/events' className='flex items-center gap-2'>
+                See more on luma
+                <ArrowRight style={{ width: 16, height: 16 }} />
+              </a>
+            </Button>
+          </div>
+        </div>
 
-      <div
-        className={`mx-[-var(--site-margin-x)] w-screen ${styles.bleedOut} ${styles.red}`}
-      ></div>
+        {events.length === 0 ? (
+          <p>No upcoming events yet.</p>
+        ) : (
+          events.map((event, index) => (
+            <div key={event._id}>
+              <EventCard event={event} index={index} />
+            </div>
+          ))
+        )}
+      </div>
     </div>
   );
 }
